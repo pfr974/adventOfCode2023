4563cdd0c7a11004a56d2432f7d935dd1b244319
Fred <pf.robert974@gmail.com>
2023-12-02 07:52:01 +1100

    quick and dirty for part1

---
+++ day1/problem1.rb
@@ -0,0 +1,17 @@
+calibration_doc = File.read("calibration_document").split
+somme = 0
+calibration_doc.each do |line| 
+  cleaned_line = line.delete('^0-9').chars
+  num = ""
+  size = cleaned_line.size
+  case size 
+  when 0
+    next
+  when 1
+    num = (cleaned_line[0] + cleaned_line[0]).to_i
+  else
+    num = (cleaned_line[0] + cleaned_line[-1]).to_i
+  end
+  somme += num
+end
+puts somme #54632
