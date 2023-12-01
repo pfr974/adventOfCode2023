3caa9cfd48f216b34a9be80f1b7eea9aea3bacf3
Fred <pf.robert974@gmail.com>
2023-12-02 08:00:09 +1100

    added part 2

--- day1/problem1.rb
+++ day1/problem1.rb
@@ -1,17 +1,21 @@
-calibration_doc = File.read("calibration_document").split
-somme = 0
-calibration_doc.each do |line| 
-  cleaned_line = line.delete('^0-9').chars
-  num = ""
-  size = cleaned_line.size
-  case size 
-  when 0
-    next
-  when 1
-    num = (cleaned_line[0] + cleaned_line[0]).to_i
-  else
-    num = (cleaned_line[0] + cleaned_line[-1]).to_i
+def somme_part1(file)
+  calibration_doc = File.read(file).split
+  somme = 0
+  calibration_doc.each do |line| 
+    cleaned_line = line.delete('^0-9').chars
+    num = ""
+    size = cleaned_line.size
+    case size 
+      when 0
+        next
+      when 1
+        num = (cleaned_line[0] + cleaned_line[0]).to_i
+      else
+        num = (cleaned_line[0] + cleaned_line[-1]).to_i
+    end
+    somme += num
   end
-  somme += num
-end
-puts somme #54632
+  somme 
+end 
+
+puts somme_part1("calibration_document")
