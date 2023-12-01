2744d0c2c4b22eea5bbbd8a46d7d999518e887d5
Fred <pf.robert974@gmail.com>
2023-12-02 09:53:24 +1100

    smarter hashes + use of lookaround regex

--- day1/problem1.rb
+++ day1/problem1.rb
@@ -21,6 +21,28 @@ HASH_CONVERSION = {
   "0" => "0",
 }
 
+HASH_CONVERSION_BIS = {
+  "on" => "1",
+  "1" => "1",
+  "tw" => "2",
+  "2" => "2",
+  "thre" => "3",
+  "3" => "3",
+  "four" => "4",
+  "4" => "4",
+  "fiv" => "5",
+  "5" => "5",
+  "six" => "6",
+  "6" => "6",
+  "seve" => "7",
+  "7" => "7",
+  "eigh" => "8",
+  "8" => "8",
+  "nin" => "9",
+  "9" => "9",
+  "0" => "0",
+}
+
 def somme_part1(file)
   calibration_doc = File.read(file).split
   somme = 0
@@ -44,7 +66,8 @@ end
 def somme_part2(file)
   calibration_doc = File.read(file).split
   somme = 0
-  regex = Regexp.union(HASH_CONVERSION.keys)
+  #regex = Regexp.union(HASH_CONVERSION_BIS.keys)
+  regex = /on(?=e)|1|tw(?=o)|2|thre(?=e)|3|four|4|fiv(?=e)|5|six|6|seve(?=n)|7|eigh(?=t)|8|nin(?=e)|9|0/
   calibration_doc.each do |line|
     num = ""
     array_content = line.scan(regex)
@@ -53,15 +76,13 @@ def somme_part2(file)
     when 0
       next 
     when 1
-      num = (HASH_CONVERSION[array_content[0]] + HASH_CONVERSION[array_content[0]]).to_i
+      num = (HASH_CONVERSION_BIS[array_content[0]] + HASH_CONVERSION_BIS[array_content[0]]).to_i
     else 
-      num = (HASH_CONVERSION[array_content[0]] + HASH_CONVERSION[array_content[-1]]).to_i
+      num = (HASH_CONVERSION_BIS[array_content[0]] + HASH_CONVERSION_BIS[array_content[-1]]).to_i
     end
     somme += num
   end
   somme
 end 
 
-
-puts somme_part1("calibration_document")
 puts somme_part2("calibration_document") #53998
--- day1/problem1_part2_example.rb
+++ day1/problem1_part2_example.rb
@@ -21,9 +21,43 @@ hash_conversion = {
   "0" => "0",
 }
 
+# Used a hint from ruby australia slack: one1fiveight should become 18 not 15!
+# If I want to keep the same approach (regex + hash), then I need to adapt the keys
+# the digits are: one, two, three, four, five, six, seven, eight, nine
+# they all share common characters at their end: e, o, e, none for four, e, none for six, n, t, e
+# So we can rewrite a different hash that still work and give 281 
+hash_conversion_bis = {
+  "on" => "1",
+  "1" => "1",
+  "tw" => "2",
+  "2" => "2",
+  "thre" => "3",
+  "3" => "3",
+  "four" => "4",
+  "4" => "4",
+  "fiv" => "5",
+  "5" => "5",
+  "six" => "6",
+  "6" => "6",
+  "seve" => "7",
+  "7" => "7",
+  "eigh" => "8",
+  "8" => "8",
+  "nin" => "9",
+  "9" => "9",
+  "0" => "0",
+}
+
+# also, I realized that my regex was not working.
+# /on|1|tw|2|thre|3|four|4|fiv|5|six|6|seve|7|eigh|8|nin|9|0/ does no work 
+# for instance if the line is 1eightw, the number are not 1,8,2 but 1,8
+# so we need to adapt it to /on(?=e)|1|tw(?=o)|2|thre(?=e)|3|four|4|fiv(?=e)|5|six|6|seve(?=n)|7|eigh(?=t)|8|nin(?=e)|9|0/
+# This came up after some google search. I didn't know about the concept of lookaround: https://stackoverflow.com/questions/2973436/regex-lookahead-lookbehind-and-atomic-groups
+
 calibration_doc = File.read("calibration_document_part2_example").split
 somme = 0
-regex = Regexp.union(hash_conversion.keys)
+regex = Regexp.union(hash_conversion_bis.keys)
+regex_bis = /on(?=e)|1|tw(?=o)|2|thre(?=e)|3|four|4|fiv(?=e)|5|six|6|seve(?=n)|7|eigh(?=t)|8|nin(?=e)|9|0/
 calibration_doc.each do |line|
   num = ""
   array_content = line.scan(regex)
@@ -32,9 +66,9 @@ calibration_doc.each do |line|
   when 0
     next 
   when 1
-    num = (hash_conversion[array_content[0]] + hash_conversion[array_content[0]]).to_i
+    num = (hash_conversion_bis[array_content[0]] + hash_conversion_bis[array_content[0]]).to_i
   else 
-    num = (hash_conversion[array_content[0]] + hash_conversion[array_content[-1]]).to_i
+    num = (hash_conversion_bis[array_content[0]] + hash_conversion_bis[array_content[-1]]).to_i
   end
   somme += num
 end
