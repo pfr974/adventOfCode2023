3eded33f848c368cd82ded72fc9713172bf6a9f5
Fred <pf.robert974@gmail.com>
2023-12-02 08:51:21 +1100

    non working (too low) part 2

--- day1/problem1.rb
+++ day1/problem1.rb
@@ -1,3 +1,26 @@
+HASH_CONVERSION = {
+  "one" => "1",
+  "1" => "1",
+  "two" => "2",
+  "2" => "2",
+  "three" => "3",
+  "3" => "3",
+  "four" => "4",
+  "4" => "4",
+  "five" => "5",
+  "5" => "5",
+  "six" => "6",
+  "6" => "6",
+  "seven" => "7",
+  "7" => "7",
+  "eight" => "8",
+  "8" => "8",
+  "nine" => "9",
+  "9" => "9",
+  "zero" => "0",
+  "0" => "0",
+}
+
 def somme_part1(file)
   calibration_doc = File.read(file).split
   somme = 0
@@ -18,4 +41,27 @@ def somme_part1(file)
   somme 
 end 
 
+def somme_part2(file)
+  calibration_doc = File.read(file).split
+  somme = 0
+  regex = Regexp.union(HASH_CONVERSION.keys)
+  calibration_doc.each do |line|
+    num = ""
+    array_content = line.scan(regex)
+    size = array_content.size 
+    case size 
+    when 0
+      next 
+    when 1
+      num = (HASH_CONVERSION[array_content[0]] + HASH_CONVERSION[array_content[0]]).to_i
+    else 
+      num = (HASH_CONVERSION[array_content[0]] + HASH_CONVERSION[array_content[-1]]).to_i
+    end
+    somme += num
+  end
+  somme
+end 
+
+
 puts somme_part1("calibration_document")
+puts somme_part2("calibration_document") #53998
