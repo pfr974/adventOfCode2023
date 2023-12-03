150fda33f8973ddb3b51f0fcef2884c5bee6b3c0
Fred <pf.robert974@gmail.com>
2023-12-04 10:48:36 +1100

    method to return adjacent cells of a matrix cell

--- day3/problem3.rb
+++ day3/problem3.rb
@@ -1,10 +1,34 @@
-require 'matrix'
+require 'matrix' # https://ruby-doc.org/stdlib-2.5.1/libdoc/matrix/rdoc/Matrix.html
 
 def read_matrix_from_input(input)
   matrix_rows = input.map { |line| line.chars }
   Matrix[*matrix_rows]
 end
 
+def get_adjacent_cells(input_matrix)
+  adjacent_cells = []
+  nb_rows = input_matrix.row_count
+  nb_cols = input_matrix.column_count
+  input_matrix.each_with_index do |cell, row, col|
+    cells = []
+    (-1..1).each do |offset_row| # allows to check left and right of a cell 
+      (-1..1).each do |offset_col| # allows to check above and below a cell  
+        next if offset_row == 0 && offset_col == 0 #allows to ignore the current cell we are looking at
+
+        check_row = row + offset_row 
+        check_col = col + offset_col
+
+        if check_row.between?(0, nb_rows - 1) && check_col.between?(0, nb_cols - 1)
+          cells << input_matrix[check_row, check_col]
+        end
+      end
+    end
+    adjacent_cells << cells
+  end
+  adjacent_cells
+end
+
+
 input_doc = File.readlines("input_example").map(&:chomp)
 input_matrix = read_matrix_from_input(input_doc)
-pp input_matrix
+pp get_adjacent_cells(input_matrix)
